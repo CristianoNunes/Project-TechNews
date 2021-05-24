e0c5fed3eb5232cc47f881442493bc87695cee17
CristianoNunes <cristianonunes99@gmail.com>
2021-05-23 23:15:53 -0300

    Requisito 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,27 @@
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    search = find_news()
+    newsPopularity = []
+    for new in search:
+        newsPopularity.append({
+            "title": new["title"],
+            "url": new["url"],
+            "popularity": new["shares_count"] + new["comments_count"]
+        })
+
+    def mostPopularity(e):
+        return e['popularity']
+
+    newsPopularity.sort(key=mostPopularity, reverse=True)
+
+    topFive = []
+    for new in newsPopularity:
+        topFive.append((new["title"], new["url"]))
+
+    return topFive[0:5]
 
 
 # Requisito 11
